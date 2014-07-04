0a9c97e5fb22701a337b2e6d35abfc489710968f
Raffael Link <r.link@proventa.de>
2014-07-04 18:46:52 +0200

    Added Option to disable OCR; Added usage information

--- jpg2pdf.sh
+++ jpg2pdf.sh
@@ -8,18 +8,38 @@
 #	     multi-page PDF. Multipage-PDF jpgs have to be named
 #	     in lexical order.
 #
+#   jpg2pdf [OPTION]... [SOURCE]
+#
+#     -c
+#        no ocr
+#
 # Output: searchable (multipage) PDF-files
 #############################################################
+usage() { echo "Usage: $0 [-c] [SOURCE]... -- convert jpg to PDF files (opt: ocr)" 1>&2; exit 1; }
 
 DIR=$1
 OCR='/opt/OCRmyPDF/'
+NOCR=false
+
+while getopts ":c" opt; do
+  case $opt in
+    c)
+      NOCR=true
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
 
 for f in $*; do
   echo Working on $f
   if [ -d "$f" ]; then
-    echo This is a directory... creating multipage PDF.
     if `convert $f/* $f.pdf`; then
-      sh $OCR/OCRmyPDF.sh -l "deu" $f.pdf $f.pdf
+      if [ "$NOCR" = false ]; then
+        sh $OCR/OCRmyPDF.sh -l "deu" $f.pdf $f.pdf
+      fi
       echo Created $f.pdf
     else
       echo ERROR: Creation of $f failed!
@@ -27,7 +47,10 @@ for f in $*; do
   else
     NAME=`echo $f | cut -d. -f1`
     if `convert $f $NAME.pdf`; then
-      sh /opt/OCRmyPDF/OCRmyPDF.sh -l "deu" $NAME.pdf $NAME.pdf
+      if [ "$NOCR" = false ]; then
+        echo OCR... NOCR= $NOCR
+        sh /opt/OCRmyPDF/OCRmyPDF.sh -l "deu" $NAME.pdf $NAME.pdf
+      fi
       echo Created $f.pdf
     else
       echo ERROR: Creation of $f failed!
