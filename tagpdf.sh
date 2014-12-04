5b58321a81fd4d1c4d92b0e949ce5549dd04dd1f
Raffael Link <r.link@proventa.de>
2014-12-04 17:13:19 +0100

    Added tag logic

--- tagpdf.sh
+++ tagpdf.sh
@@ -1,1 +1,19 @@
-pdftotext *.pdf - | tr [:space:] '\n' | grep -v -E '[0-9]' | grep -Eo '[a-zA-Z]+' | grep -e '[^\ ]\{3,\}' | grep -ivwFf top10000de.txt | sort | uniq -ci | sort -rn | head -n 40 | column
+#!/bin/sh
+
+files=`find $* -maxdepth 1 -print0 | xargs -0 -eof tmsu tags | grep ": \$" | tr -d ':'`
+
+for file in $files; do
+
+  mupdf $file 2>/dev/null &
+  mupdf_pid=$!
+
+  read -p "Tags: " tags
+  tmsu tag $file $tags
+
+  kill $mupdf_pid
+  wait $mupdf_pid 2>/dev/null
+
+done
+
+
+# pdftotext *.pdf - | tr [:space:] '\n' | grep -v -E '[0-9]' | grep -Eo '[a-zA-Z]+' | grep -e '[^\ ]\{3,\}' | grep -ivwFf top10000de.txt | sort | uniq -ci | sort -rn | head -n 40 | column
